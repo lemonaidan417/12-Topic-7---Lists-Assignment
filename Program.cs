b9bfdb871b5bc4e9c843b09d577efe9c70ffb9ba
Aidan Sutherland <339501421@GoTVDSB.ca>
2024-04-09 14:41:33 -0400

    not done

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace _12_Topic_7___Lists_Assignment
             Console.WriteLine(string.Join(", ", list));
 
             string choice = "";
-            int valueAdd, userOccurance;
+            int valueAdd, userOccurrence;
 
             while (choice != "q")
             {
@@ -31,11 +31,11 @@ namespace _12_Topic_7___Lists_Assignment
                 Console.WriteLine("2 - Make a new list of random numbers");
                 Console.WriteLine("3 - Remove a number (by value)");
                 Console.WriteLine("4 - Add a value to the list");
-                Console.WriteLine("5 - Count the number of occurrences of a specified number");
+                Console.WriteLine("5 - Count the number of occurrences of a specified number (Not done)");
                 Console.WriteLine("6 - Print the largest value");
                 Console.WriteLine("7 - Print the smallest value");
-                Console.WriteLine("8 - Sum and Average"); // Bonus
-                Console.WriteLine("9 - Determine the most frequently occurring value(s)"); // Bonus
+                Console.WriteLine("8 - Sum and Average (Not done)"); // Bonus
+                Console.WriteLine("9 - Determine the most frequently occurring value(s) (Not done)"); // Bonus
                 Console.WriteLine("q - Quit");
                 Console.WriteLine();
                 choice = Console.ReadLine().ToLower().Trim();
@@ -94,9 +94,9 @@ namespace _12_Topic_7___Lists_Assignment
                 else if (choice == "5")
                 {
                     Console.WriteLine("You chose Count the number of occurrences of a specified number");
-                    userOccurance = Convert.ToInt32(Console.ReadLine());
+                    userOccurrence = Convert.ToInt32(Console.ReadLine());
                     
-                }
+                } // Not Done
                 else if (choice == "6")
                 {
                     Console.WriteLine("You chose Print the largest value");
@@ -108,18 +108,17 @@ namespace _12_Topic_7___Lists_Assignment
                     Console.WriteLine("You chose Print the smallest value");
                     int min = list.Min();
                     Console.WriteLine($"The smallest value is {min}");
-                } // Done
+                } // Don
                 else if (choice == "8")
                 {
                     Console.WriteLine("You chose");
 
-                } // Bonus
+                } // Bonus (Didn't do)
                 else if (choice == "9")
                 {
                     Console.WriteLine("You chose");
 
-                } // Bonus
-                // Add an else for each valid choice...
+                } // Bonus (Didn't do)
                 else
                 {
                     Console.WriteLine("Invalid choice, press ENTER to continue.");
@@ -132,13 +131,72 @@ namespace _12_Topic_7___Lists_Assignment
             Console.WriteLine("Welcome to my List of String assignment." +
                 "\nI will now create a list of 5 of my favourite vegetables.");
             List<string> vegList = new List<string>();
-            vegList.Add("1 - GREEN ONION");
-            vegList.Add("2 - GREEN BEANS");
-            vegList.Add("3 - LETTUCE");
-            vegList.Add("4 - POTATO");
-            vegList.Add("5 - BROCCOLI");
+            string vegChoice = "";
+            vegList.Add("GREEN ONION");
+            vegList.Add("GREEN BEANS");
+            vegList.Add("LETTUCE");
+            vegList.Add("POTATO");
+            vegList.Add("BROCCOLI");
+            Console.WriteLine();
             Console.WriteLine("Vegetables");
-            Console.WriteLine(string.Join("\n", vegList));
+            // Console.WriteLine(string.Join("\n", $"{vegList} - ", vegList));
+            vegList.ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Please select what you would like to to do to the list:");
+            Console.WriteLine();
+            Console.WriteLine("1 - Remove a vegetable by index");
+            Console.WriteLine("2 - Remove a vegetable by value");
+            Console.WriteLine("3 - Search for a vegetable");
+            Console.WriteLine("4 - Add a vegetable");
+            Console.WriteLine("5 - Sort list");
+            Console.WriteLine("6 - Clear the list");
+            Console.WriteLine("q - Quit");
+            Console.WriteLine();
+            vegChoice = Console.ReadLine();
+            while (vegChoice != "q")
+            {
+                if (vegChoice == "1")
+                {
+                    Console.WriteLine("You chose Remove a vegetable by index");
+                    Console.WriteLine("Please input the index of the vegetable you would like to remove: ");
+                    int vegRemoveInt = int.Parse(Console.ReadLine());
+                    vegList.RemoveAt((vegRemoveInt - 1));
+                    Console.WriteLine(string.Join("\n", vegList));
+                }
+                else if (vegChoice == "2")
+                {
+                    Console.WriteLine("You chose Remove a vegetable by value");
+                    Console.WriteLine("Please input the value of the vegetable you would like to remove: ");
+                    string vegRemoveVal = Console.ReadLine().ToLower().Trim();
+                    vegList.Remove(vegRemoveVal);
+                    Console.WriteLine(string.Join("\n", vegList));
+                }
+                else if (vegChoice == "3")
+                {
+                    Console.WriteLine("You chose Search for a vegetable");
+                }
+                else if (vegChoice == "4")
+                {
+                    Console.WriteLine("You chose Add a vegetable");
+                }
+                else if (vegChoice == "5")
+                {
+                    Console.WriteLine("You chose Sort the list");
+                }
+                else if (vegChoice == "6")
+                {
+                    Console.WriteLine("You chose Clear the list");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, press ENTER to continue.");
+                    Console.ReadLine();
+                }
+            }
         }
         static void Main(string[] args)
         {
